9bc371b246b0fd9cc667a6fd6d7f93567f07d0e8
Conor MB <c.monaghanbrowne1@universityofgalway.ie>
2024-10-09 16:08:52 +0100

    task 8 completed

--- task8.py
+++ task8.py
@@ -1,2 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+sinx = np.load('/home/pi/ee347/lab-4-advanced-python-group-10/task7_sin.npy')
+cosx = np.load('/home/pi/ee347/lab-4-advanced-python-group-10/task7_cos.npy')
+
+x = np.linspace(0, 10, 101)
+
+plt.plot(x, sinx, label='sin(x)')
+
+plt.plot(x, cosx, label='cos(x)')
+
+plt.legend()
+plt.xlabel('x')
+plt.ylabel('y')
+
+plt.show()
